33432d87f80fdff71804653d4f117664e59b6326
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-26 03:16:13 +0300

    1

--- src/Part_2.java
+++ src/Part_2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Part_2 {
+public class Part_2 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5 , 6 , 7 , 8 , 9};
+        int sum = arraySum(array, 0); // Starts to sum up from the first element with index 0
+        System.out.println("Sum of array elements: " + sum);
+    }
+
+    public static int arraySum(int[] array, int i) {
+        //  If the index i is equal to the array length, return 0
+        if (i == array.length) {
+            return 0;
+        }
+
+        // Calculate the sum of elements from index i to the end of the array
+        int currentElement = array[i];
+        int restOfTheArraySum = arraySum(array, i + 1);
+
+        // Return the sum of the current element and the sum of the rest of the array
+        return currentElement + restOfTheArraySum;
+    }
 }
+
--- src/Part_3.java
+++ src/Part_3.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Part_3 {
+public class Part_3 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 2, 4, 2, 5 , 4 , 8 , 9 , 5 , 4 , 1 , 7 , 3};
+        int target = 3; // The value of number which we are searching for
+        int count = findCountInArray(array, target, 0); // Start searching from the first element with index 0
+        System.out.println("Count of " + target + " in the array: " + count);
+    }
+
+    public static int findCountInArray(int[] array, int what, int i) {
+        //  If the index i is equal to the array length, return 0
+        if (i == array.length) {
+            return 0;
+        }
+
+        // Check if the current element at index i is equal to the given value
+        if (array[i] == what) {
+            // If equal, increment count and continue searching in the rest of the array
+            return 1 + findCountInArray(array, what, i + 1);
+        } else {
+            // If not equal, continue searching in the rest of the array
+            return findCountInArray(array, what, i + 1);
+        }
+    }
 }
+
