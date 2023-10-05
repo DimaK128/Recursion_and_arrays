06377d82fbd01d952be90fb58a8f7388dc849e57
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-10-05 11:16:44 +0300

    1

--- src/Part_1.java
+++ src/Part_1.java
@@ -1,15 +1,15 @@
 public class Part_1 {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5 , 6 , 7 , 8 , 9};
-        printArray(array, 0);     // Prints array with elements 1,2,3,4,5,6.... which is starting with index 0
+        printArray(array, 0);
     }
 
     public static void printArray(int[] array, int i) {
-        // It checks if index i is equal to the length of the array.
+        //
         if (i >= array.length) {
             return;
         }
-        System.out.print(array[i] + " ");  // Prints current element with index i
-        printArray(array, i + 1);        // Recursion to print the next element
+        System.out.print(array[i] + " ");
+        printArray(array, i + 1);
     }
 }
